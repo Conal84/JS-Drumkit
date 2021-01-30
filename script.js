6d9654827bd238a173c1e9b6e44f3700fdd66524
Conal Walsh <conal.walsh1@gmail.com>
2021-01-30 17:54:32 +0000

    Refactored code into a playSOund function

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 "use strict";
 
-window.addEventListener("keydown", function (e) {
-  console.log(e);
+// A function to play key sound and add playing class
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  key.classList.add("playing");
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-});
+}
+
+// Remove playing class on transition end
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; // skip if not a transform event
+  this.classList.remove("playing");
+}
+
+window.addEventListener("keydown", playSound);
+
+// Get all keys and add transition end event listeners
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
